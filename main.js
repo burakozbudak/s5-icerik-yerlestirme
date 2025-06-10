2362ff37989f75f64ab120e8a82a6a9d9b9cf53d
burakozbudak <burakozbudak25@hacettepe.edu.tr>
2025-06-11 00:12:18 +0300

    head implated

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ import { siteContent } from "./content.js";
 console.log("Site içeriği", siteContent);
 
 /* Kodlar buradan aşağıya */
+const htmlTemplate = {
+  head: {
+    title: "Dijital Pazarlama Uzmanı",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1.0" }
+    ],
+    links: [
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" },
+      { rel: "stylesheet", href: "reset.css" },
+      { rel: "stylesheet", href: "styles.css" }
+    ]
+  },
